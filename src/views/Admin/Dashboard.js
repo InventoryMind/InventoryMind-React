d3f750c19b963db072372dcc9a2d294a2f7ea004
thuva9872 <64319324+thuva9872@users.noreply.github.com>
2021-11-10 12:41:27 +0530

    Update Dashboard.js

--- src/views/Admin/Dashboard.js
+++ src/views/Admin/Dashboard.js
@@ -30,7 +30,7 @@ export default function Dashboard() {
   const classes = useStyles();
   const [data,setData]=useState();
   useEffect(()=>{
-    fetch(process.env.REACT_APP_API+'/admin/getDashboardData',{credentials:'include',headers:{Cookie:"auth-token="+JSON.parse(localStorage.token).token}})
+    fetch(process.env.REACT_APP_API+'/admin/getDashboardData',{credentials:'include'})
      .then(response => response.json())
     .then(data=>setData(data.msg))
     .catch(e=>console.log(e));
